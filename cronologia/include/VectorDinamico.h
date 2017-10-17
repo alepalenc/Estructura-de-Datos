b043a9466d7ba308ccb0bd0de48e90573b9e870b
Inma <32613495+InmaMC@users.noreply.github.com>
2017-10-17 09:24:25 +0200

    Update VectorDinamico.h

--- cronologia/include/VectorDinamico.h
+++ cronologia/include/VectorDinamico.h
@@ -14,12 +14,12 @@
  *
  *
  * Una instancia @e v del tipo de datos abstracto @c VectorDinamico sobre el
- * tipo @c T es un array 1-dimensional de un determinado tamaño @e n, que
- * puede crecer y decrecer a petición del usuario. Lo podemos representar como
+ * tipo @c T es un array 1-dimensional de un determinado tamaÃ±o @e n, que
+ * puede crecer y decrecer a peticiÃ³n del usuario. Lo podemos representar como
  *
  *  {v[0],v[1],...,v[n-1]} 
  *
- * donde v[i] es el valor almacenado en la posición i del vector
+ * donde v[i] es el valor almacenado en la posiciÃ³n i del vector
  *
  * La eficiencia en espacio es @e O(n).
  */
@@ -29,44 +29,44 @@ class VectorDinamico {
     /**
       * @page repVectorDinamico Rep del TDA VectorDinamico
       *
-      * @section invVectorDinamico Invariante de la representación
+      * @section invVectorDinamico Invariante de la representaciÃ³n
       *
-      * Un objeto válido @e v del TDA VectorDinamico debe cumplir 
+      * Un objeto vÃ¡lido @e v del TDA VectorDinamico debe cumplir 
       * - @c v.ocupados >= 0
       * - @c v.nelementos >= @c v.ocupados
       * - @c v.datos apunta a una zona de memoria con capacidad para albergar
       *   @c v.nelementos valores de tipo @c T
       *
-      * @section faVectorDinamico  Función de abstracción
+      * @section faVectorDinamico  FunciÃ³n de abstracciÃ³n
       *
-      * Un objeto válido @e rep del TDA VectorDinamico representa al vector de
-      * tamaño @e nelementos, de los cuales @e ocupados elementos están en uso
+      * Un objeto vÃ¡lido @e rep del TDA VectorDinamico representa al vector de
+      * tamaÃ±o @e nelementos, de los cuales @e ocupados elementos estÃ¡n en uso
       *
       * {v.datos[0],v.datos[1],...,v.datos[ocupados-1],...,v.datos[v.nelementos-1]}
       *
       */
     T * datos; /**< Apunta a los elementos del vector */
-    int nelementos; /**< Indica el número de elementos en @c datos */
-		int ocupados; /**< Indica el número de elementos en uso en @c datos */
+    int nelementos; /**< Indica el nÃºmero de elementos en @c datos */
+	int ocupados; /**< Indica el nÃºmero de elementos en uso en @c datos */
   
   
   public:
   // ---------------  Constructores ----------------
   /**
     * @brief Constructor por defecto
-    * @param n indica el número de componentes inicial reservadas
+    * @param n indica el nÃºmero de componentes inicial reservadas
     *   para el vector
     * @note
-    *   Este constructor también corresponde al de por defecto
+    *   Este constructor tambiÃ©n corresponde al de por defecto
     */
     VectorDinamico(int n=0);
   
   /**
     * @brief Constructor de copia
     * @param c.datos vector de datos
-    * @param c.nelementos número de componentes inicial reservadas
+    * @param c.nelementos nÃºmero de componentes inicial reservadas
     *   para el vector
-    * @param c.ocupados número de componentes inicial ocupadas
+    * @param c.ocupados nÃºmero de componentes inicial ocupadas
     *   en el vector  
     */
     VectorDinamico(const VectorDinamico<T> & original);
@@ -76,79 +76,79 @@ class VectorDinamico {
 
   // --------------- Otras funciones ---------------
   /**
-    * @brief Número de componentes del vector
-    * @return Devuelve el número de componentes que puede almacenar en este
+    * @brief NÃºmero de componentes del vector
+    * @return Devuelve el nÃºmero de componentes que puede almacenar en este
     * instante el vector
     * @see resize()
     */
     int getNelementos() const;
 
 	/**
-    * @brief Número de componentes en uso del vector
-    * @return Devuelve el número de componentes que están usándose en este
+    * @brief NÃºmero de componentes en uso del vector
+    * @return Devuelve el nÃºmero de componentes que estÃ¡n usÃ¡ndose en este
     * instante en el vector
     */
     int getOcupados() const;
   
   /**
-    * @brief Comprueba si el vector está vacío
+    * @brief Comprueba si el vector estÃ¡ vacÃ­o
     */
     bool vacio() const;
   
   /**
-    * @brief Comprueba si el vector está lleno
+    * @brief Comprueba si el vector estÃ¡ lleno
     */
     bool lleno() const;
 
   /**
     * @brief Acceso a un elemento de un vector
-    * @param i la posición del vector donde está el componente
+    * @param i la posiciÃ³n del vector donde estÃ¡ el componente
     * @pre 0<=i<getOcupados()
     * @return La referencia al elemento. Por tanto, se puede usar para almacenar
-    *     un valor en esa posición.
+    *     un valor en esa posiciÃ³n.
     */
     T & operator[] (int i);
 
   /**
     * @brief Acceso a un elemento de un vector constante
-    * @param i la posición del vector donde está el componente
+    * @param i la posiciÃ³n del vector donde estÃ¡ el componente
     * @pre 0<=i<getOcupados()
     * @return La referencia al elemento. Se supone que el vector no se puede
-        modificar y por tanto es acceso de sólo lectura
+        modificar y por tanto es acceso de sÃ³lo lectura
     */
     const T & operator[](int i) const;
 
   /**
-    * @brief Redimensión del vector
-    * @param n El nuevo tamaño del vector. n>=0
-    * @post Los valores almacenados antes de la redimensión no se pierden
-    *  (excepto los que se salen del nuevo rango de índices)
+    * @brief RedimensiÃ³n del vector
+    * @param n El nuevo tamaÃ±o del vector. n>=0
+    * @post Los valores almacenados antes de la redimensiÃ³n no se pierden
+    *  (excepto los que se salen del nuevo rango de Ã­ndices)
     */
     void resize(int n);
     
   /**
-    * @brief Operador de asignación
+    * @brief Operador de asignaciÃ³n
     * @param c.datos vector de datos
-    * @param c.nelementos número de componentes inicial reservadas
+    * @param c.nelementos nÃºmero de componentes inicial reservadas
     *   para el vector
-    * @param c.ocupados número de componentes inicial ocupadas
+    * @param c.ocupados nÃºmero de componentes inicial ocupadas
     *   en el vector  
     */
     VectorDinamico & operator=(const VectorDinamico<T> & original);
   
   /**
-    * @brief Añade un nuevo elemento
-    * @param nuevo el nuevo elemento a añadir
-    * @param i la posición en la que se va a añadir el nuevo elemento 
-    *   (por defecto justo detrás del último)
+    * @brief AÃ±ade un nuevo elemento
+    * @param nuevo el nuevo elemento a aÃ±adir
+    * @param i la posiciÃ³n en la que se va a aÃ±adir el nuevo elemento 
+    *   (por defecto justo detrÃ¡s del Ãºltimo)
     * @pre 0<=i<=getOcupados()
     */ 
     void insertar(const T & nuevo, int i=getOcupados());
 
   /**
     * @brief Elimina un elemento
-    * @param i la posición del elemento que se va a eliminar
-    *   (por defecto el último)
+    * @param i la posiciÃ³n del elemento que se va a eliminar
+    *   (por defecto el Ãºltimo)
     * @pre 0<=i<getOcupados()
     */ 
     void eliminar(int i=getOcupados()-1);

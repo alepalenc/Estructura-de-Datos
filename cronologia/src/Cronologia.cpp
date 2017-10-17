451d8cc32401a863c6c23020243d6c4150820af2
Inma <32613495+InmaMC@users.noreply.github.com>
2017-10-17 09:23:51 +0200

    Update Cronologia.cpp

--- cronologia/src/Cronologia.cpp
+++ cronologia/src/Cronologia.cpp
@@ -30,7 +30,7 @@ void Cronologia::insertar(const FechaHistorica &fech){
 	while(cronol[i].anio<aux)
 		i++;
 	if(cronol[i] == aux){
-		//////////
+		cronol[i] += fech.hechos;
 	}else{
 		cronol.insertar(fech, i);
 	}
@@ -42,13 +42,59 @@ void Cronologia::insertarPrincipio(const FechaHistorica &fech){
 	insertar(fech, 0);
     }
 
+//Consultar si anio
+int Cronologia::consultarAnio(int fech){
+	int pos = -1;
+	int n = cronol.getOcupados();
+	int i=0;
+	bool encontrado = false;
+	while(i<ocupados && encontrado == false){
+		if(cronol[i].anio == fech){
+			encontrado = true;
+		}else{
+			i++;
+		}
+	}
+	if(encontrado == true){
+		pos = i;
+	}
+	return pos;
+}
+
+
+//Consultar si existe un hecho y en que anio ocurrió
+int Cronologia::consultarHecho(const string & h){
+	int pos=-1;
+	int n=cronol.getOcupados();
+	for()
+	
+	for (int i=0 ; i<n && pos==-1 ; ++i){
+		int m = cronol[i].hechos.getOcupados();
+		for(int j=0; j<m && pos==-1; ++j){
+			if (h==cronol[i].hechos[j])
+				pos=i;
+		}
+	}
+		
+	return pos;
+}
+		
+
 //Eliminar
 void Cronologia::eliminar(int pos){
 	cronol.eliminar(pos);
 }
 
+//Eliminar anio en concreto
+void Cronologia::eliminarFecha(int fech){
+	int pos = consultarAnio(fech);
+	if(pos != -1){
+		cronol.eliminar(pos);
+	}
+}	
+
 //Comprobar que está vacia
-bool Cronologia::vacia(){
+bool Cronologia::vacia() const{
 	return cronol == 0;
 }
 
